f532271e83700231e29369fee2b30a0ee4149c7f
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2019-10-02 18:04:17 +0300

    feat: add my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        //Нижний обновляемый диапазон чисел
+        this.min = 0;
+        //Верхний обновляемый диапазон чисел
+        this.max = 0;
+        //Предположительное число
+        this.number = 0;
+    }
 
     setRange(min, max) {
-
+        //Задаём минимальную начальную границу чисел
+        this.min = min;
+        //Задаём максимальную начальную границу чисел
+        this.max = max;
+        //Начинаем поиск с максимального числа
+        this.number = max;
     }
 
     guess() {
-
+        if(this.number === this.max) {
+            this.number = this.max - (this.max - this.min)/2
+        };
+        return this.number;
+      
     }
 
     lower() {
-
+        this.max = this.number;
+        this.number = Math.ceil(this.max - (this.max - this.min)/2);
     }
 
     greater() {
-
+        this.min = this.number;
+        this.number = Math.ceil(this.min + (this.max - this.min)/2);
     }
 }
 
